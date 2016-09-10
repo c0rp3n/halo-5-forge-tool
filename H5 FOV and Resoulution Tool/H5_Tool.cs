0be6ff2c981a04dd4b700fafa43e59c4dfb3f784
Corpen's Den <ojhitchcock@gmail.com>
2016-09-10 18:33:45 +0100

    First test build
    This build has the ability to read and write but the addressing wont
    work as I cant get the base addresses right but using cheat engine to
    generate it currently works.

---
+++ H5 FOV and Resoulution Tool/H5_Tool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace H5_FOV_and_Resoulution_Tool
+{
+    public partial class H5_Tool : Form
+    {
+        public H5_Tool()
+        {
+            InitializeComponent();
+
+            uint addr = 0x009FCE85;
+            
+            byte[] fov = BitConverter.GetBytes( 50.0 );
+
+            MemoryManager.WriteToAddress(addr, fov);
+        }
+    }
+}
